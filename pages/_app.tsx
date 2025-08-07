3d31e62c88e5a354f6102e9c8248740502d56239
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-08-07 23:09:06 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import "../styles/index.css";
 import "../styles/checkout.css";
 import "../styles/results.css";
 import "../styles/products.css";
+import "../styles/home.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
