9ad5e181677d4dd48cb3e102cbbc14333aa51162
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-08-07 21:08:39 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
+import "../styles/index.css";
+import "../styles/checkout.css";
+import "../styles/results.css";
+import "../styles/products.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
